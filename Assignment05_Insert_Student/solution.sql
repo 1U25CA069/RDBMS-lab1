a63651940c46e6bc9efaa83cd034ca8ba9de0f3e
1U25CA069 <priyankasivakumar778@gmail.com>
2026-07-17 11:13:01 +0530

    Update solution.sql

--- Assignment05_Insert_Student/solution.sql
+++ Assignment05_Insert_Student/solution.sql
@@ -1,4 +1,7 @@
--- Insert
+--use collegeDBbca;
+CREATE TABLE coursecourseID VARCHAR(5),courseName varchar(30),credits INT(5) PRIMARY KEY,departmentID VARCHAR(2));
+INSERT INTO course(courseID,coureName,credits,departmentID) Values(1,"BCA",10,2);
+SELECT * from course; Insert
 
 -- 1001 Arun
 
