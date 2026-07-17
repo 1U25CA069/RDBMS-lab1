250c00f16229495b81d855c6f0b92cbea94b9ada
1U25CA069 <priyankasivakumar778@gmail.com>
2026-07-17 11:11:58 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,4 +1,11 @@
--- Create Course table
+-create database course123;
+use course123;
+
+create table course(courseID integer(10) primary key,coursename varchar(40),credit integer(20),DepartmentID integer(30));
+describe course;
+
+insert course value("10001","CS","10000","105"),("10003","CS","10002","105"),("10002","CS","10001","105");
+select*from course;- Create Course table
 
 -- Insert three records
 
