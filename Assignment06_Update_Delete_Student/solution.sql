7930551716ca8af95df0df567b82cfbd05523cb4
1U25CA069 <priyankasivakumar778@gmail.com>
2026-07-17 11:14:37 +0530

    Update solution.sql

--- Assignment06_Update_Delete_Student/solution.sql
+++ Assignment06_Update_Delete_Student/solution.sql
@@ -1,4 +1,12 @@
--- Create database if required
+-- create table db;
+use db;
+create table marksheet(Rollno integer(20) primary key,stuname varchar(20),mark integer(4),depID integer(20));
+desc student;
+insert  marksheet values("1001","prithi","80","101");
+
+insert  marksheet values("1002","niru","80","101");
+insert  marksheet values("1003","chandru","70","101");
+insert  marksheet values("1004","muthu","85","101");Create database if required
 
 -- Create Student table
 
