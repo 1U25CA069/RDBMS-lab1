ad8f99507c3331e9e029abb25a5eaf3215441255
1U25CA069 <priyankasivakumar778@gmail.com>
2026-07-17 11:09:59 +0530

    Update solution.sql

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -1,3 +1,15 @@
+USE departmentDB;
+CREATE TABLE Student(
+StudentID  int(5)primary key,          
+StudentName VARCHAR(20)NOT NULL,
+DOB DATE NOT NULL,
+Gender VARCHAR(10) NOT NULL,
+DepartmentID INT (5) NOT NULL,
+CONSTRAINT UQ_studentName UNIQUE KEY(StudentName),
+CONSTRAINT FK_DepartmentID FOREIGN KEY (DepartmentID)
+REFERENCES Department(DepartmentID)
+);
+DESC Student;
 -- Create Student table
 
 -- StudentID
