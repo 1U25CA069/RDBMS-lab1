0f0919db3711ed64e04c9600f6ae6cfe6e69decd
1U25CA069 <priyankasivakumar778@gmail.com>
2026-09-01 10:18:49 +0530

    Update solution.sql

--- Assignment08_Aggregate_Functions/solution.sql
+++ Assignment08_Aggregate_Functions/solution.sql
@@ -1,11 +1,16 @@
--- Create Employee table
-
--- Insert records
-
--- COUNT()
-
--- MAX()
-
--- MIN()
-
--- AVG()
+use collegeDBbca;
+CREATE TABLE employee(
+      employeeID INT PRIMARY KEY,
+      employeename VARCHAR(50),
+      department VARCHAR(50),
+      salary int
+);
+INSERT INTO employee(employeeID,employeename,department,salary) VALUES(101,'RAVI','HR',25000);
+INSERT INTO employee(employeeID,employeename,department,salary) VALUES(102,'MEENA','IT',40000);
+INSERT INTO employee(employeeID,employeename,department,salary) VALUES(103,'KUMAR','FINANCE',35000);
+INSERT INTO employee(employeeID,employeename,department,salary) VALUES(104,'SURESH','IT',45000);
+INSERT INTO employee(employeeID,employeename,department,salary) VALUES(105,'LATHA','HR',30000);
+SELECT COUNT(salary)AS total_employees FROM employee;
+SELECT MAX(salary)AS highest_salary FROM employee;
+SELECT MIN(salary)AS lowest_salary FROM employee;
+SELECT AVG(salary)AS.average_salary FROM employee;
